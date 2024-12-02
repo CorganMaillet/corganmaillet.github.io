c0188e45eb2cf672825160d8f31f827ddc33e523
Corgan <corgan.maillet@snhu.edu>
2024-12-01 23:32:18 -0600

    Hopefully fix directory

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  basePath: "/index",
+  output: "export",
   reactStrictMode: true,
-  basePath: 'corganmaillet.github.io' 
 }
 
 module.exports = nextConfig
